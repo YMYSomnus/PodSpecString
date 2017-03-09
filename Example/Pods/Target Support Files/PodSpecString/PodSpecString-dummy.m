c17009a3c65297d85467f779057fb3b82997d9e3
yangmingyue <yangmingyue713@jk.cn>
2017-03-09 10:54:45 +0800

    add PodSpecString Demo
    Signed-off-by: yangmingyue <yangmingyue713@jk.cn>

---
+++ Example/Pods/Target Support Files/PodSpecString/PodSpecString-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodSpecString : NSObject
+@end
+@implementation PodsDummy_PodSpecString
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodSpecString_Example/Pods-PodSpecString_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodSpecString_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PodSpecString_Example
+@end
